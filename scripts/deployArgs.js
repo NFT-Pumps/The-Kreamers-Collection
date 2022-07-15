9cfd553a5509e554a1ab63e264dbe7daa9186bf5
greene5952 <dustin@wolf-solutions.us>
2022-07-14 19:02:39 -0500

    Update deployArgs.js

--- scripts/deployArgs.js
+++ scripts/deployArgs.js
@@ -12,15 +12,15 @@ module.exports = [
         ethers.utils.getAddress('0x8912e40d291693C7A86C418D52b46766780C6989'),
         ethers.utils.getAddress('0x6b3ba9F0475C646FDaF2Bc207C285a485B88A747'),
         ethers.utils.getAddress('0xbE400D9A6f8140B51dE4B3b7AAb1DA4B9adec9a0'),
-        // ethers.utils.getAddress('0xC3ea7494c33AbCA4FcEa01331043311756b7e336'),
-        // ethers.utils.getAddress('0xC3ea7494c33AbCA4FcEa01331043311756b7e336')
+        ethers.utils.getAddress('0xEAC3ce292F95d779732e7a26c95c57A742cf5119'),
+        ethers.utils.getAddress('0xC3ea7494c33AbCA4FcEa01331043311756b7e336')
     ],
     [
         26,
         12,
         10,
         6,
-        // 5,
-        // 40
+        5,
+        40
     ]
 ]
