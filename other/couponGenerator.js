1e2a789dbbedb459423dd04cd24a93e3a155db18
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-08-16 14:39:13 -0500

    wl

--- other/couponGenerator.js
+++ other/couponGenerator.js
@@ -22,11 +22,13 @@ async function getClaimCodes() {
     //const [owner, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20] = await ethers.getSigners();
 
     let presaleAddresses = [
-        { address : '0x9Dc7D54E10A199C00176FFA68459fD9b15A706c4', qty : 5},
-        { address : '0xE742Ce21b06bd9dCDFb5d5fE2C7F75BC889B9836', qty : 2},
-        { address : '0xe2a377F6C8222663bF965534Fcd48eeF9DCA4bE0', qty : 2},
-        { address : '0xb6b0Eda826F7D427558A110ff26024d580a0feDa', qty : 2},
-        { address : '0xCd43AdcB61949ab14D3f4574BFbDA53d46389715', qty : 1}       
+        { address : '0x9Dc7D54E10A199C00176FFA68459fD9b15A706c4', qty : 10},
+        { address : '0xE742Ce21b06bd9dCDFb5d5fE2C7F75BC889B9836', qty : 4},
+        { address : '0xe2a377F6C8222663bF965534Fcd48eeF9DCA4bE0', qty : 4},
+        { address : '0xA9d7f2401f19Cd61b162E9c608E5d028c2B3c84d', qty : 4},
+        { address : '0xb6b0Eda826F7D427558A110ff26024d580a0feDa', qty : 4},
+        { address : '0x742510aa0160bD8C9774102aa09317216158d0Ed', qty : 4},
+        { address : '0xCd43AdcB61949ab14D3f4574BFbDA53d46389715', qty : 12}       
     ]      
     
     function createCoupon(hash, signerPvtKey) {
